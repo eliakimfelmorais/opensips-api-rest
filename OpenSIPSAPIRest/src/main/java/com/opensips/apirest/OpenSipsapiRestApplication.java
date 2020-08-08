7da873dca9b61ce46605f2e86544ff904294b4d9
Eliakim Feliciano Morais <eliakimfelmorais@gmail.com>
2020-08-08 19:53:53 -0300

    adicao de projeto spring boot

---
+++ OpenSIPSAPIRest/src/main/java/com/opensips/apirest/OpenSipsapiRestApplication.java
@@ -0,0 +1,13 @@
+package com.opensips.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSipsapiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenSipsapiRestApplication.class, args);
+	}
+
+}
---
+++ OpenSIPSAPIRest/src/test/java/com/opensips/apirest/OpenSipsapiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensips.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSipsapiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
